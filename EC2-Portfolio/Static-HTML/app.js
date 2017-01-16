f03b8adb240fd6e747bc51b281402746c8ff114e
LongPotato <bkh4nh@gmail.com>
2017-01-16 14:09:07 -0800

    static html page

---
+++ EC2-Portfolio/Static-HTML/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
